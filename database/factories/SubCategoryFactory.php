05c2009492682f621fb856eeea54c26a3cdb63ea
Ariful <arif98741@gmail.com>
2020-07-08 09:45:47 +0600

    changes

---
+++ database/factories/SubCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\SubCategory;
+use Faker\Generator as Faker;
+
+$factory->define(SubCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,31 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'Admin\Auth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'Admin\Auth\LoginController@login');
+  Route::post('/logout', 'Admin\Auth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'Admin\Auth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'Admin\Auth\RegisterController@register');
+
+  Route::post('/password/email', 'Admin\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'Admin\Auth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'Admin\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'Admin\Auth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'customer'], function () {
+  Route::get('/login', 'Customer\Auth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'Customer\Auth\LoginController@login');
+  Route::post('/logout', 'Customer\Auth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'Customer\Auth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'Customer\Auth\RegisterController@register');
+
+  Route::post('/password/email', 'Customer\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'Customer\Auth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'Customer\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'Customer\Auth\ResetPasswordController@showResetForm');
+});
